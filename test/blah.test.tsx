b109228dc950272d2063bd36eb8bb3d5a5e9f0ae
simbathesailor <anilchaudhary453@gmail.com>
2019-11-16 20:49:34 +0530

    fixing test case

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { useWhatChanged } from '../src/useWhatChanged';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<div>Hello world</div>, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+  it('should be defined', () => {
+    expect(useWhatChanged).toBeDefined();
   });
 });
